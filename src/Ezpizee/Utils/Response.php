c19733f5f786189b30de2bc2cae6f867512f8507
Sothea Nim <sothea.nim@ezpizee.com>
2021-01-06 10:49:43 -0500

    format

--- src/Ezpizee/Utils/Response.php
+++ src/Ezpizee/Utils/Response.php
@@ -7,80 +7,80 @@ use RuntimeException;
 
 class Response implements JsonSerializable
 {
-  private $method = '';
-  private $endpoint = '';
-  private $data = [
-    'status'  => ResponseCodes::STATUS_SUCCESS,
-    'code'    => ResponseCodes::CODE_SUCCESS,
-    'message' => ResponseCodes::MESSAGE_SUCCESS,
-    'data'    => []
-  ];
+    private $method = '';
+    private $endpoint = '';
+    private $data = [
+        'status'  => ResponseCodes::STATUS_SUCCESS,
+        'code'    => ResponseCodes::CODE_SUCCESS,
+        'message' => ResponseCodes::MESSAGE_SUCCESS,
+        'data'    => []
+    ];
 
-  public function __construct(string $method, string $endpoint, string $buffer)
-  {
-    $this->method = $method;
-    $this->endpoint = $endpoint;
-    $this->data['data'] = new ListModel([]);
+    public function __construct(string $method, string $endpoint, string $buffer)
+    {
+        $this->method = $method;
+        $this->endpoint = $endpoint;
+        $this->data['data'] = new ListModel([]);
 
-    if (EncodingUtil::isValidJSON($buffer)) {
-      $data = json_decode(str_replace(["\n", "\r", "\t"], '', $buffer), true);
-      foreach ($data as $k => $item) {
-        if (isset($this->data[$k])) {
-          $this->data[$k] = $item;
+        if (EncodingUtil::isValidJSON($buffer)) {
+            $data = json_decode(str_replace(["\n", "\r", "\t"], '', $buffer), true);
+            foreach ($data as $k => $item) {
+                if (isset($this->data[$k])) {
+                    $this->data[$k] = $item;
+                }
+            }
+            $this->data['data'] = new ListModel($this->data['data']);
         }
-      }
-      $this->data['data'] = new ListModel($this->data['data']);
+        else {
+            throw new RuntimeException('JSON_STRING_REQUIRED_FOR_BUFFER', ResponseCodes::CODE_ERROR_INTERNAL_SERVER);
+        }
     }
-    else {
-      throw new RuntimeException('JSON_STRING_REQUIRED_FOR_BUFFER', ResponseCodes::CODE_ERROR_INTERNAL_SERVER);
+
+    public function getStatus()
+    : string
+    {
+        return $this->data['status'];
     }
-  }
 
-  public function getStatus()
-  : string
-  {
-    return $this->data['status'];
-  }
-
-  public function getMessage()
-  : string
-  {
-    return $this->data['message'];
-  }
-
-  public function getData()
-  : ListModel
-  {
-    if (is_null($this->data['data'])) {
-      return new ListModel([]);
+    public function getMessage()
+    : string
+    {
+        return $this->data['message'];
     }
-    else if ($this->data['data'] instanceof ListModel) {
-      if ($this->data['data']->has(0) && $this->data['data']->get(0) === null || $this->data['data']->get(0) === 'null') {
-        return new ListModel([]);
-      }
+
+    public function getData()
+    : ListModel
+    {
+        if (is_null($this->data['data'])) {
+            return new ListModel([]);
+        }
+        else if ($this->data['data'] instanceof ListModel) {
+            if ($this->data['data']->has(0) && $this->data['data']->get(0) === null || $this->data['data']->get(0) === 'null') {
+                return new ListModel([]);
+            }
+        }
+        return $this->data['data'];
     }
-    return $this->data['data'];
-  }
 
-  public function isError()
-  : bool
-  {
-    return $this->getCode() !== 200;
-  }
+    public function isError()
+    : bool
+    {
+        return $this->getCode() !== 200;
+    }
 
-  public function getCode()
-  : int
-  {
-    return (int)($this->data['code'] . '');
-  }
+    public function getCode()
+    : int
+    {
+        return (int)($this->data['code'] . '');
+    }
 
-  public function jsonSerialize()
-  {
-    return ['method' => $this->method, 'endpoint' => $this->endpoint, 'data' => $this->data];
-  }
+    public function jsonSerialize()
+    {
+        return ['method' => $this->method, 'endpoint' => $this->endpoint, 'data' => $this->data];
+    }
 
-  public function __toString()
-  {
-    return json_encode(['method' => $this->method, 'endpoint' => $this->endpoint, 'data' => $this->data]);
-  }
+    public function __toString()
+    {
+        return json_encode(['method' => $this->method, 'endpoint' => $this->endpoint, 'data' => $this->data]);
+    }
 }
--- src/Ezpizee/Utils/UUID.php
+++ src/Ezpizee/Utils/UUID.php
@@ -4,19 +4,19 @@ namespace Ezpizee\Utils;
 
 class UUID
 {
-  private function __construct()
-  {
-  }
+    private function __construct()
+    {
+    }
 
-  public static final function id()
-  : string
-  {
-    return EncodingUtil::uuid();
-  }
+    public static final function id()
+    : string
+    {
+        return EncodingUtil::uuid();
+    }
 
-  public static final function isValid(string $id)
-  : bool
-  {
-    return EncodingUtil::isValidUUID($id);
-  }
+    public static final function isValid(string $id)
+    : bool
+    {
+        return EncodingUtil::isValidUUID($id);
+    }
 }
