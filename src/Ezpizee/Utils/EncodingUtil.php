5346ad0888b1b93d93de483e7fb4c3e70740772c
Sothea Nim <sothea.nim@ezpizee.com>
2022-01-01 15:02:51 -0500

    improved uuid

--- src/Ezpizee/Utils/EncodingUtil.php
+++ src/Ezpizee/Utils/EncodingUtil.php
@@ -5,7 +5,8 @@ namespace Ezpizee\Utils;
 final class EncodingUtil
 {
     private static $MD5_REGEX = '/^[a-f0-9]{32}$/';
-    private static $UUID_V4_REGEX = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+    private static $UUID_V4_REGEX1 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+    private static $UUID_V4_REGEX2 = '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
 
     public static function isBase64Encoded($val)
     : bool
@@ -28,13 +29,13 @@ final class EncodingUtil
     public static final function uuid()
     : string
     {
-        return strtoupper(exec('uuidgen'));
+        return strtolower(exec('uuidgen'));
     }
 
     public static final function isValidUUID(string $id)
     : bool
     {
-        return preg_match(self::$UUID_V4_REGEX, $id) === 1;
+        return preg_match(self::$UUID_V4_REGEX1, $id) === 1 || preg_match(self::$UUID_V4_REGEX2, $id) === 1;
     }
 
     public static function jsonDecode(array &$arr)
