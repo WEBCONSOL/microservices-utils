a1e1721cbe298b8c868755b8e76262f373289ae7
Sothea Nim <sothea.nim@ezpizee.com>
2022-01-13 08:55:06 +0700

    updated

--- src/Ezpizee/Utils/EncodingUtil.php
+++ src/Ezpizee/Utils/EncodingUtil.php
@@ -29,7 +29,15 @@ final class EncodingUtil
     public static final function uuid()
     : string
     {
-        return strtolower(exec('uuidgen'));
+        $uuid = exec('uuidgen');
+        if (empty($uuid)) {
+            $data = random_bytes(16);
+            assert(strlen($data) == 16);
+            $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+            $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+            $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+        }
+        return strtolower($uuid);
     }
 
     public static final function isValidUUID(string $id)
