7cb87e5e1e29145aca02190f80e055e9aa787316
Sothea Nim <sothea.nim@ezpizee.com>
2022-03-13 14:27:15 -0400

    improved uuid

--- src/Ezpizee/Utils/EncodingUtil.php
+++ src/Ezpizee/Utils/EncodingUtil.php
@@ -4,11 +4,17 @@ namespace Ezpizee\Utils;
 
 final class EncodingUtil
 {
-    private static $MD5_REGEX = '/^[a-f0-9]{32}$/';
-    private static $UUID_V4_REGEX1 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
-    private static $UUID_V4_REGEX1_2 = '/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i';
-    private static $UUID_V4_REGEX2 = '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
-    private static $UUID_V4_REGEX2_2 = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9A-F]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i';
+    private static $UUID_TYPE           = 'alphanumericUUID';
+    private static $MD5_REGEX           = '/^[a-f0-9]{32}$/';
+    private static $UUID_V4_REGEX1      = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+    private static $UUID_V4_REGEX1_2    = '/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i';
+    private static $UUID_V4_REGEX2      = '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
+    private static $UUID_V4_REGEX2_2    = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9A-F]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i';
+    private static $UUID_REGEX3         = '/[a-z]\d[a-z]\d[a-z]\d[a-z]\d[a-z]\d[a-z]\d$/i';
+    private static $NUMERICS            = [0,1,2,3,4,5,6,7,8,9];
+    private static $ALPHABETS           = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    private static $NUM_ALPHABETS       = 25;
+    private static $NUM_NUMERICS        = 9;
 
     public static function isBase64Encoded($val)
     : bool
@@ -31,22 +37,46 @@ final class EncodingUtil
     public static final function uuid()
     : string
     {
-        $uuid = exec('uuidgen');
-        if (empty($uuid)) {
-            $data = random_bytes(16);
-            assert(strlen($data) == 16);
-            $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-            $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-            $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-        }
-        return strtolower($uuid);
+        return self::$UUID_TYPE === 'alphanumericUUID' ? self::alphanumericUUID() : self::v4uuid();
+    }
+
+    private static function v4uuid()
+    : string
+    {
+        $data = random_bytes(16);
+        assert(strlen($data) == 16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    private static function alphanumericUUID()
+    : string
+    {
+        $n1 = rand(0, self::$NUM_ALPHABETS);
+        $n2 = rand(0, self::$NUM_NUMERICS);
+        $n3 = rand(0, self::$NUM_ALPHABETS);
+        $n4 = rand(0, self::$NUM_NUMERICS);
+        $n5 = rand(0, self::$NUM_ALPHABETS);
+        $n6 = rand(0, self::$NUM_NUMERICS);
+        $n7 = rand(0, self::$NUM_ALPHABETS);
+        $n8 = rand(0, self::$NUM_NUMERICS);
+        $n9 = rand(0, self::$NUM_ALPHABETS);
+        $n10 = rand(0, self::$NUM_NUMERICS);
+        $n11 = rand(0, self::$NUM_ALPHABETS);
+        $n12 = rand(0, self::$NUM_NUMERICS);
+        return self::$ALPHABETS[$n1].self::$NUMERICS[$n2].self::$ALPHABETS[$n3].
+            self::$NUMERICS[$n4].self::$ALPHABETS[$n5].self::$NUMERICS[$n6].
+            self::$ALPHABETS[$n7].self::$NUMERICS[$n8].self::$ALPHABETS[$n9].
+            self::$NUMERICS[$n10].self::$ALPHABETS[$n11].self::$NUMERICS[$n12];
     }
 
     public static final function isValidUUID(string $id)
     : bool
     {
         return preg_match(self::$UUID_V4_REGEX1, $id) === 1 || preg_match(self::$UUID_V4_REGEX2, $id) === 1 ||
-            preg_match(self::$UUID_V4_REGEX1_2, $id) === 1 || preg_match(self::$UUID_V4_REGEX2_2, $id) === 1;
+            preg_match(self::$UUID_V4_REGEX1_2, $id) === 1 || preg_match(self::$UUID_V4_REGEX2_2, $id) === 1 ||
+            (strlen($id) === 12 && preg_match(self::$UUID_REGEX3, $id) === 1);
     }
 
     public static function jsonDecode(array &$arr)
