62c989b181a3935a77d5c744071c41066d818926
Sothea Nim <sothea.nim@ezpizee.com>
2022-03-20 17:01:54 -0400

    improved encryption

--- src/Ezpizee/Utils/UUID.php
+++ src/Ezpizee/Utils/UUID.php
@@ -5,6 +5,6 @@ namespace Ezpizee\Utils;
 class UUID
 {
     private function __construct(){}
-    public static function id(): string {return EncodingUtil::uuid();}
+    public static function id(int $max=12): string {return EncodingUtil::uuid($max);}
     public static function isValid(string $id): bool {return EncodingUtil::isValidUUID($id);}
 }
