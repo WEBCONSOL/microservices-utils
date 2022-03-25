98e951d58335604af8c34747d4b7ca5491ae2efb
Sothea Nim <sothea.nim@ezpizee.com>
2022-03-25 17:36:19 -0400

    updated

--- src/Ezpizee/Utils/Constants.php
+++ src/Ezpizee/Utils/Constants.php
@@ -28,4 +28,6 @@ class Constants
     const HEADER_AUTHORIZATION_BASIC_PFX = "Basic ";
     const EZPZ_FAKE_PHONE_PFX = "ezpznotphone";
     const EZPZ_FAKE_EMAIL_PFX = "ezpznotemail";
+    const NUMERICS = [0,1,2,3,4,5,6,7,8,9];
+    const ALPHABETS = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z'];
 }
--- src/Ezpizee/Utils/EncodingUtil.php
+++ src/Ezpizee/Utils/EncodingUtil.php
@@ -10,41 +10,34 @@ final class EncodingUtil
     private static $UUID_V4_REGEX2      = '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
     private static $UUID_V4_REGEX2_2    = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9A-F]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i';
     private static $UUID_REGEX3         = '[a-zA-B]\d[a-zA-B]\d[a-zA-B]\d';
-    private static $NUMERICS            = [0,1,2,3,4,5,6,7,8,9];
-    private static $ALPHABETS           = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z'];
 
-    public static function isBase64Encoded($val)
-    : bool
+    public static function isBase64Encoded($val): bool
     {
         return is_string($val) && base64_encode(base64_decode($val, true)) === $val;
     }
 
-    public static function isValidJSON($str)
-    : bool
+    public static function isValidJSON($str): bool
     {
         return $str && is_string($str) && is_array(json_decode($str, true)) && (json_last_error() == JSON_ERROR_NONE);
     }
 
-    public static function isValidMd5(string $md5)
-    : bool
+    public static function isValidMd5(string $md5): bool
     {
         return preg_match(self::$MD5_REGEX, $md5) === 1;
     }
 
-    public static function uuid(int $max=12)
-    : string
+    public static function uuid(int $max=12): string
     {
         $rands = [];
-        $n1 = sizeof(self::$ALPHABETS) - 1;
-        $n2 = sizeof(self::$NUMERICS) - 1;
+        $n1 = sizeof(Constants::ALPHABETS) - 1;
+        $n2 = sizeof(Constants::NUMERICS) - 1;
         for($i=0; $i<$max; $i++) {
-            $rands[] = $i%2 === 0 ? self::$ALPHABETS[rand(0, $n1)] : self::$NUMERICS[rand(0, $n2)];
+            $rands[] = $i%2 === 0 ? Constants::ALPHABETS[rand(0, $n1)] : Constants::NUMERICS[rand(0, $n2)];
         }
         return implode('', $rands);
     }
 
-    public static function v4uuid()
-    : string
+    public static function v4uuid(): string
     {
         $data = random_bytes(16);
         assert(strlen($data) == 16);
@@ -53,8 +46,7 @@ final class EncodingUtil
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
-    public static final function isValidUUID(string $id)
-    : bool
+    public static final function isValidUUID(string $id): bool
     {
         // v4uuid
         if (preg_match(self::$UUID_V4_REGEX1, $id) === 1 || preg_match(self::$UUID_V4_REGEX2, $id) === 1 ||
@@ -70,8 +62,7 @@ final class EncodingUtil
         return false;
     }
 
-    public static function jsonDecode(array &$arr)
-    : void
+    public static function jsonDecode(array &$arr): void
     {
         foreach ($arr as $i=>$v) {
             if (is_array($v)) {
